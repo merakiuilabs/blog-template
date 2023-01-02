930078c46d6d8ba8201872a8b254adfd927d5b7c
khatabwedaa <khatabwedaa@gmail.com>
2023-01-02 17:34:30 +0200

    Update footer

--- src/App.vue
+++ src/App.vue
@@ -385,10 +385,69 @@
         </div>
     </section>
 
-    <footer class="bg-white dark:bg-gray-900">
-        <div class="container px-6 py-8 mx-auto">
-            <div class="text-center">
-                <a class="text-2xl font-medium text-sky-500 transition-colors flex justify-center items-center duration-300 transform dark:text-sky-400 hover:text-sky-400 dark:hover:text-sky-300" href="#">
+    <footer class="bg-white mt-12 dark:bg-gray-900">
+        <div class="container px-4 py-12 mx-auto">
+            <div class="md:flex md:-mx-3 md:items-center md:justify-between">
+                <h1 class="text-3xl font-semibold tracking-tight text-gray-800 md:mx-3 xl:text-4xl dark:text-white">Subscribe our newsletter to get update.</h1>
+                
+                <div class="mt-6 md:mx-3 shrink-0 md:mt-0 md:w-auto">
+                    <a href="#" class="inline-flex items-center justify-center w-full px-4 py-2 text-sm text-white duration-300 bg-sky-500 rounded-lg hover:bg-sky-600 focus:ring focus:ring-sky-300 focus:ring-opacity-80">
+                        <span class="mx-2">Sign Up Now</span>
+
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mx-2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
+                        </svg>
+                    </a>
+                </div>
+            </div>
+            
+            <hr class="my-6 border-gray-200 md:my-10 dark:border-gray-700">
+
+            <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                <div>
+                    <p class="font-semibold text-gray-800 dark:text-white">Quick Link</p>
+
+                    <div class="flex flex-col items-start mt-5 space-y-2">
+                        <a href="#" class="text-gray-600 transition-colors duration-300 dark:text-gray-300 dark:hover:text-blue-400 hover:underline hover:text-blue-500">Home</a>
+                        <a href="#" class="text-gray-600 transition-colors duration-300 dark:text-gray-300 dark:hover:text-blue-400 hover:underline hover:text-blue-500">Who We Are</a>
+                        <a href="#" class="text-gray-600 transition-colors duration-300 dark:text-gray-300 dark:hover:text-blue-400 hover:underline hover:text-blue-500">Our Philosophy</a>
+                    </div>
+                </div>
+
+                <div>
+                    <p class="font-semibold text-gray-800 dark:text-white">Industries</p>
+
+                    <div class="flex flex-col items-start mt-5 space-y-2">
+                        <a href="#" class="text-gray-600 transition-colors duration-300 dark:text-gray-300 dark:hover:text-blue-400 hover:underline hover:text-blue-500">Retail & E-Commerce</a>
+                        <a href="#" class="text-gray-600 transition-colors duration-300 dark:text-gray-300 dark:hover:text-blue-400 hover:underline hover:text-blue-500">Information Technology</a>
+                        <a href="#" class="text-gray-600 transition-colors duration-300 dark:text-gray-300 dark:hover:text-blue-400 hover:underline hover:text-blue-500">Finance & Insurance</a>
+                    </div>
+                </div>
+
+                <div>
+                    <p class="font-semibold text-gray-800 dark:text-white">Services</p>
+
+                    <div class="flex flex-col items-start mt-5 space-y-2">
+                        <a href="#" class="text-gray-600 transition-colors duration-300 dark:text-gray-300 dark:hover:text-blue-400 hover:underline hover:text-blue-500">Translation</a>
+                        <a href="#" class="text-gray-600 transition-colors duration-300 dark:text-gray-300 dark:hover:text-blue-400 hover:underline hover:text-blue-500">Proofreading & Editing</a>
+                        <a href="#" class="text-gray-600 transition-colors duration-300 dark:text-gray-300 dark:hover:text-blue-400 hover:underline hover:text-blue-500">Content Creation</a>
+                    </div>
+                </div>
+
+                <div>
+                    <p class="font-semibold text-gray-800 dark:text-white">Contact Us</p>
+
+                    <div class="flex flex-col items-start mt-5 space-y-2">
+                        <a href="#" class="text-gray-600 transition-colors duration-300 dark:text-gray-300 dark:hover:text-blue-400 hover:underline hover:text-blue-500">+880 768 473 4978</a>
+                        <a href="#" class="text-gray-600 transition-colors duration-300 dark:text-gray-300 dark:hover:text-blue-400 hover:underline hover:text-blue-500">info@merakiui.com</a>
+                    </div>
+                </div>
+            </div>
+            
+            <hr class="my-6 border-gray-200 md:my-10 dark:border-gray-700">
+            
+            <div class="flex flex-col items-center justify-between sm:flex-row">
+                <a class="text-2xl font-medium text-sky-500 transition-colors flex items-center duration-300 transform dark:text-sky-400 hover:text-sky-400 dark:hover:text-sky-300" href="#">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.7" stroke="currentColor" class="w-8 h-8">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 7.125C2.25 6.504 2.754 6 3.375 6h6c.621 0 1.125.504 1.125 1.125v3.75c0 .621-.504 1.125-1.125 1.125h-6a1.125 1.125 0 01-1.125-1.125v-3.75zM14.25 8.625c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v8.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 01-1.125-1.125v-8.25zM3.75 16.125c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v2.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 01-1.125-1.125v-2.25z" />
                     </svg>
@@ -396,33 +455,7 @@
                     <h3 class="mx-2">Blog</h3>
                 </a>
 
-                <p class="max-w-md mx-auto mt-4 text-gray-500 dark:text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-
-                <div class="flex flex-col mt-6 sm:flex-row sm:items-center sm:justify-center">
-                    <button class="order-1 w-full px-5 py-2.5 mt-3 text-sm tracking-wide text-gray-600 capitalize transition-colors duration-300 transform border rounded-md sm:mx-2 dark:border-gray-400 dark:text-gray-300 sm:mt-0 sm:w-auto hover:bg-gray-50 focus:outline-none focus:ring dark:hover:bg-gray-800 focus:ring-gray-300 focus:ring-opacity-40">
-                        <div class="flex items-center justify-center -mx-1">
-                            <svg class="w-5 h-5 mx-1" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C21.9939 17.5203 17.5203 21.9939 12 22ZM4 12.172C4.04732 16.5732 7.64111 20.1095 12.0425 20.086C16.444 20.0622 19.9995 16.4875 19.9995 12.086C19.9995 7.68451 16.444 4.10977 12.0425 4.086C7.64111 4.06246 4.04732 7.59876 4 12V12.172ZM10 16.5V7.5L16 12L10 16.5Z" fill="currentColor"></path>
-                            </svg>
-    
-                            <span class="mx-1">View Demo</span>
-                        </div>
-                    </button>
-
-                    <button class="w-full px-5 py-2.5 text-sm tracking-wide text-white capitalize transition-colors duration-300 transform bg-sky-500 rounded-md sm:mx-2 sm:order-2 sm:w-auto hover:bg-sky-400 focus:outline-none focus:ring focus:ring-sky-300 focus:ring-opacity-80">Get started</button>
-                </div>
-            </div>
-
-            <div class="flex flex-col mt-10 md:mt-20 items-center sm:flex-row sm:justify-between">
-                <p class="text-sm text-gray-600">© Copyright 2021. All Rights Reserved.</p>
-
-                <div class="flex mt-3 -mx-3 sm:mt-0">
-                    <a href="#" class="mx-3 text-sm text-gray-600 transition-colors duration-300 hover:text-gray-500 dark:hover:text-gray-300" aria-label="Reddit"> Teams </a>
-
-                    <a href="#" class="mx-3 text-sm text-gray-600 transition-colors duration-300 hover:text-gray-500 dark:hover:text-gray-300" aria-label="Reddit"> Privacy </a>
-
-                    <a href="#" class="mx-3 text-sm text-gray-600 transition-colors duration-300 hover:text-gray-500 dark:hover:text-gray-300" aria-label="Reddit"> Cookies </a>
-                </div>
+                <p class="mt-4 text-sm text-gray-500 sm:mt-0 dark:text-gray-300">© Copyright 2021. All Rights Reserved.</p>
             </div>
         </div>
     </footer>
